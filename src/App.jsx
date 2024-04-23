78472bc37319e5ff241c7b7dcf8c32c4b3506208
sina-prvnd <96621439+sina-prvnd@users.noreply.github.com>
2024-04-23 19:04:26 +0330

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>services</section>
+      <section>Parallax</section>
+      <section>portfolio1</section>
+      <section>portfolio2</section>
+      <section>portfolio3</section>
+      <section>contact</section>
+    </div>
+  );
+};
+
+export default App;
